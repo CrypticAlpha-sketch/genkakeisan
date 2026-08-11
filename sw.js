bf52f85fc14fd205569f59ddf7b7e6e7e52d0722
CrypticAlpha-sketch <120298331+CrypticAlpha-sketch@users.noreply.github.com>
2026-08-11 09:36:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* 職人原価計算 ─ オフライン用 */
+const CACHE = "shokunin-genka-v3";
+const ASSETS = ["./","./index.html","./manifest.webmanifest",
+  "./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+/* 画面はキャッシュ優先。裏で新しい版を取り直す */
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(hit => {
+    const net = fetch(e.request).then(res => {
+      if (res && res.status === 200 && res.type === "basic")
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+      return res;
+    }).catch(() => hit);
+    return hit || net;
+  }));
+});
